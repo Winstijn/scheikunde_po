533c734ee696f24ae01140d6e50f50accddaf5b8
winstijn <winstijn@hotmail.nl>
2023-11-22 10:47:05 +0100

    Added files for new deployment on polymer2

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,9 @@
 if (/darwin/ =~ RUBY_PLATFORM) == nil
- bind "unix:///var/run/sockets/alkwin_scheikunde.sock"
- daemonize true
+# bind "unix:///var/run/sockets/alkwin_scheikunde.sock"
+ port 3000
 else
  port ENV.fetch("PORT") { 3000 }
 end
+
 environment "production"
 pidfile 'tmp/pids/server.pid'
